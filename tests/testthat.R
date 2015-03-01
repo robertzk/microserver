ffee28e5f5499bb50c4b648d212b84d2923192c6
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-02-28 22:29:32 -0600

    wip on file reading test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,4 @@
 library("testthat")
+library("testthatsomemore")
 library("microserver")
 test_check("microserver")
--- tests/testthat/test-routes.r
+++ tests/testthat/test-routes.r
@@ -1,22 +1,39 @@
-context('determine_route')
+context("determine_route")
 
-test_that('it determines the root 404 route for a trivial example', {
-  expect_identical(determine_route(list(), '')()$status, 404)
+test_that("it determines the root 404 route for a trivial example", {
+  expect_identical(determine_route(list(), "")()$status, 404)
 })
 
-test_that('it determines the ping route for a simple example', {
-  expect_identical(determine_route(list('/ping' = function() 'Hello world'), '/ping')(),
-                                   'Hello world')
+test_that("it determines the ping route for a simple example", {
+  expect_identical(determine_route(list("/ping" = function() "Hello world"), "/ping")(),
+                                   "Hello world")
 })
 
-test_that('it determines the root 404 route for a simple example', {
+test_that("it determines the root 404 route for a simple example", {
   expect_identical(determine_route(
-    list('/ping' = function() 'Hello world'), '/notping')()$status, 404)
+    list("/ping" = function() "Hello world"), "/notping")()$status, 404)
 })
 
-test_that('it determines the root route for a simple example', {
-  expect_identical(determine_route(list('/ping' = function() 'Hello world',
-                                        function() 'Index page'), '/notping')(),
-                                   'Index page')
+test_that("it determines the root route for a simple example", {
+  expect_identical(determine_route(list("/ping" = function() "Hello world",
+                                        function() "Index page"), "/notping")(),
+                                   "Index page")
 })
 
+test_that("it cannot fetch the file for an asset if it doesn't exist", {
+  expect_identical(determine_route(list(serve_static = TRUE), "index.html")()$status, 404)
+})
+
+test_that("it fetches the file for an asset if it exists", {
+  within_file_structure(list(public = list("index.html" = "I am a real file!")), {
+    resp <- in_dir(tempdir, determine_route(list(serve_static = TRUE), "index.html"))()
+    # expect_identical(resp$body, "I am a real file!")
+    pending()
+  })
+})
+
+package_stub("stats", "lm", function(...) "I'm a model", {
+  # Within the confines of these braces, lm is temporarily
+  # a stubbed function. It will be restored when this test is over.
+  expect_identical(lm(Sepal.Width ~ Sepal.Height, iris), "I'm a model")
+  })
