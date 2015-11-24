a3ffb2bce1408212a88c23b10aea4cbca11f0ea8
Peter Hurford <peter@peterhurford.com>
2015-11-23 20:38:15 -0500

    Double quotes

--- R/response.r
+++ R/response.r
@@ -5,23 +5,23 @@
 #'
 #' @param status integer. HTTP status (default is \code{200}).
 #' @param headers list. A list of HTTP headers (default is
-#'    \code{list('Content-Type' = 'text/json')}.
+#'    \code{list("Content-Type" = "text/json")}.
 #' @export
 #' @examples
 #' \dontrun{
-#' response('Did not work!', 404) # 404 error
+#' response("Did not work!", 404) # 404 error
 #' response(list(a = 1, b = 2))   # { "a": 1, "b": 2 } JSON response
 #' }
 microserver_response <- function(response = NULL, status = 200,
-                     headers = list('content-type' = 'text/json')) {
+                     headers = list("content-type" = "text/json")) {
   response <- tryCatch(
     list(body    = response_body(response, headers),
          status  = status,
          headers = headers),
-    error = function(err) list(body = to_json(list(status = 'error',
-      message = 'Error parsing JSON response')), status = 500,
-      headers = list('content-type' = 'text/json')))
-  class(response) <- 'microserver_response'
+    error = function(err) list(body = to_json(list(status = "error",
+      message = "Error parsing JSON response")), status = 500,
+      headers = list("content-type" = "text/json")))
+  class(response) <- "microserver_response"
   response
 }
 
@@ -30,7 +30,7 @@ microserver_response <- function(response = NULL, status = 200,
 #' @param obj any R object.
 #' @return \code{TRUE} or \code{FALSE} if \code{obj} inherits \code{response}.
 #' @seealso \code{\link{microserver_response}}
-is.microserver_response <- function(obj) { inherits(obj, 'microserver_response') }
+is.microserver_response <- function(obj) { inherits(obj, "microserver_response") }
 
 
 response_body <- function(response, headers) {
