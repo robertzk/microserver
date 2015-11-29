19f99fe6b5d573b341b0a603d907fb739a28a18f
Peter Hurford <peter@peterhurford.com>
2015-11-28 20:36:24 -0500

    is.microserver_response test

--- tests/testthat/test-response.r
+++ tests/testthat/test-response.r
@@ -31,3 +31,12 @@ test_that("it returns a 200 response and gives raw html", {
   expect_identical(resp$status, 200)
   expect_identical(resp$body, "hello I am <p>HTML</p>")
 })
+
+describe("is.microserver_response", {
+  test_that("returns FALSE if it is not a microserver response", {
+    expect_false(is.microserver_response("hi"))
+  })
+  test_that("returns TRUE if it is a microserver response", {
+    expect_true(is.microserver_response(microserver_response("hi")))
+  })
+})
