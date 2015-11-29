e40b7090d7af7485efbd3d2dd4ca7db24a2513cf
Peter Hurford <peter@peterhurford.com>
2015-11-28 20:21:24 -0500

    Run the html example

--- R/html.r
+++ R/html.r
@@ -2,9 +2,7 @@
 #'
 #' @param html_string character. A sring of HTML.
 #' @examples
-#' \dontrun{
-#'    routes <- list("/index" = function(...) html("<b>bold!</b>"))
-#' }
+#' routes <- list("/index" = function(...) html("<b>bold!</b>"))
 #' @export
 html <- function(html_string) {
   microserver_response(html_string, headers = list("content-type" = "text/html"))
